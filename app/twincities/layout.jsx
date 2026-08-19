c1c646c03014fb200d80dcec9c2642579958d754
babiikingburd-pixel <babiikingburd@gmail.com>
2026-08-19 08:23:28 -0500

    Show live system state and pending work across Twin Cities

--- app/twincities/layout.jsx
+++ app/twincities/layout.jsx
@@ -1,10 +1,12 @@
 import ClientSafetyNet from "./ClientSafetyNet";
 import SwarmHeartbeat from "./SwarmHeartbeat";
+import SystemStatusBar from "./SystemStatusBar";
 
 export default function TwinCitiesLayout({ children }) {
   return (
     <>
       <ClientSafetyNet />
+      <SystemStatusBar />
       <SwarmHeartbeat />
       {children}
     </>
