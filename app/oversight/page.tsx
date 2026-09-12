d7462c0d353bc339bec8a51633bc0d63439b62b9
babiikingburd-pixel <babiikingburd@gmail.com>
2026-09-12 00:24:14 -0500

    Load Superb holographic Oversight theme

--- app/oversight/page.tsx
+++ app/oversight/page.tsx
@@ -4,6 +4,8 @@ import { auditProperty } from "@/lib/oversight/doctor";
 import { loadOversightConsoleData } from "@/lib/oversight/consoleData";
 import "./oversight.css";
 import "./cockpit-mode.css";
+import "./superb.css";
+import "./holo-deck.css";
 export const dynamic = "force-dynamic";
 
 export default async function OversightPage() {
